a440a323719e568713af8c00556e8974dd378e26
Mila <milanaganaga@yandex.ru>
2023-06-22 16:13:21 +0400

    Mesto Express

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const router = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true,
-});
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
+  // eslint-disable-next-line no-console
+  .then(() => console.log('Connected to DB'))
+  // eslint-disable-next-line no-console
+  .catch((err) => console.log('Server Connection Error!!!\n', err));
 
+// mongoose
+//   .connect('mongodb://127.0.0.1:27017/mestodb')
+//   .then(() => console.log('Connected to DB'))
+//   .catch((err) => console.log('Server Connection Error!!!\n', err));
 app.use(express.json());
 
 app.use((req, res, next) => {
