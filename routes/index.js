89be6b47afd8610f381b11436d2e113ca6db8e7d
Mila <milanaganaga@yandex.ru>
2023-07-06 18:47:13 +0400

    14

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,25 @@ const router = require('express').Router();
 
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
+
+const { createUser, login } = require('../controllers/users');
+
+const { validationLogin, validationCreateUser } = require('../middlewares/validation');
+
 const NotFoundError = require('../errors/NotFoundError');
+
+const auth = require('../middlewares/auth');
+
+router.use(auth);
+
+router.post('/signup', createUser, validationCreateUser);
+router.post('/signin', login, validationLogin);
+
+router.use(() => {
+  throw new NotFoundError('Страница не найдена');
+});
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
-router.use('*', (req, res, next) => next(new NotFoundError('Не найдено')));
 
 module.exports = router;
