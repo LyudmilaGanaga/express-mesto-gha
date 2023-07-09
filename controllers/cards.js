bc3bb92bbee43c0f80799e627298492ef207b14d
Mila <milanaganaga@yandex.ru>
2023-07-10 01:42:22 +0400

    мангус валидация и функции карточек

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,63 +5,87 @@ const NotFoundError = require('../errors/NotFoundError');
 const BadRequest = require('../errors/BadRequest');
 const UnauthorizedCardDeleteException = require('../errors/UnauthorizedCardDeleteException');
 
-async function getCards(req, res, next) {
-  try {
-    const cards = await Card.find({});
-    res.send(cards);
-  } catch (err) {
-    next(err);
-  }
-}
-
-async function createCard(req, res, next) {
-  try {
-    const { name, link } = req.body;
-    const ownerId = req.user._id;
-    const card = await Card.create({ name, link, owner: ownerId });
-    res.status(201).send(card);
-  } catch (err) {
-    if (err.name === 'CastError' || err.name === 'BadRequest') {
-      next(new BadRequest('BadRequest'));
-      return;
-    }
-    next(err);
-  }
-}
-const deleteCard = (req, res, next) => {
-  Card.findById(req.params.cardId)
-    .orFail(new NotFoundError('Card not found'))
-    .then((card) => {
-      if (card.owner.toString() !== req.user._id) {
-        throw new UnauthorizedCardDeleteException('UnauthorizedCardDeleteException');
-      }
-      return Card.findByIdAndRemove(req.params.cardId);
-    })
-    .then((card) => res.send(card))
+const getCards = (req, res, next) => {
+  Card
+    .find({})
+    .populate('owner')
+    .then((card) => res.status(200).send({ data: card }))
     .catch((err) => {
-      if (err instanceof mongoose.Error.CastError) {
-        return next(new BadRequest('BadRequest'));
-      }
-      return next(err);
+      next(err);
     });
 };
 
-async function likeCard(req, res, next) {
-  try {
-    const userId = req.user._id;
-    const card = await Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $addToSet: { likes: userId } },
-      { new: true },
-    );
-    if (!card) {
+const createCard = (req, res, next) => {
+  const { name, link } = req.body;
+
+  Card
+    .create(
+      {
+        name,
+        link,
+        owner: req.user._id,
+      },
+    )
+    .then((card) => res.status(201).send({ data: card }))
+
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        next(new BadRequest('BadRequest'));
+      } else {
+        next(err);
+      }
+    });
+};
+const deleteCard = (req, res, next) => {
+  const { cardId } = req.params;
+  Card
+    .findById(cardId)
+    .orFail(() => {
       throw new NotFoundError('Card not found');
-    }
-    res.send(card);
-  } catch (err) {
-    next(err);
-  }
-}
+    })
+    .then((card) => {
+      const owner = card.owner.toString();
+      const _id = req.user._id.toString();
+      if (owner === _id) {
+        Card.deleteOne(card)
+          .then(() => {
+            res.status(200).send({ message: 'ОК' });
+          })
+          .catch(next);
+      } else {
+        throw new UnauthorizedCardDeleteException('UnauthorizedCardDeleteException');
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequest('BadRequest'));
+      } else {
+        next(err);
+      }
+    });
+};
+const likeCard = (req, res, next) => {
+  const { cardId } = req.params;
+
+  Card.findByIdAndUpdate(
+    cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .populate(['owner', 'likes'])
+    .orFail(() => {
+      throw new NotFoundError('Card not found');
+    })
+    .then((card) => res.status(200).send({ data: card, message: 'ОК' }))
+
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequest('BadRequest'));
+      } else {
+        next(err);
+      }
+    });
+};
 
 async function dislikeCard(req, res, next) {
   try {
